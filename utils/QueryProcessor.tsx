e494792a916da3fdf6e12e911384826347513941
Alexandra Scharpf <ascharpf@andrew.cmu.edu>
2026-02-24 10:17:40 -0500

    added multiplied query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -28,6 +28,22 @@ export default function QueryProcessor(query: string): string {
       return largest.toString();
     }
   }
+  // if (query.toLowerCase().includes("square and a cube")){
+  //   const numbers = query.match(/\d+/g);
+  //   if (numbers && numbers.length >= 2) {
+  //     for i in 0..numbers.length {
+  //     const square = Math.pow(parseInt(numbers[0]), 2);
+  //     const cube = Math.pow(parseInt(numbers[1]), 3);
+  //     return (square + cube).toString();
+  //   }
+  // }
+  if (query.toLowerCase().includes("multiplied")) {
+    const numbers = query.match(/\d+/g);
+    if (numbers && numbers.length >= 2) {
+      const product = parseInt(numbers[0]) * parseInt(numbers[1]);
+      return product.toString();
+    }
+  }
 
   return "";
 }
